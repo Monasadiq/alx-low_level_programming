df71be4d92a8e81305de3f244d07114e70ac67e6
monasadiq <mounsadiq759@gmail.com>
2023-04-18 12:03:48 -0700

    0x0d

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: (cm-amos)Amos Mwongela
+ * Desc: Header file that defines a macro ABS(x)
+ *       that computes the absolus valus on number x
+ */
+
+#define  ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
