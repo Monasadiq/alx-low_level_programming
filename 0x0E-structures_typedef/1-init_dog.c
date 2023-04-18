a90aed55108cb72224aa3d0709d54ac72a9174af
monasadiq <mounsadiq759@gmail.com>
2023-04-18 06:39:33 -0700

    0x0e

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,6 +6,8 @@
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
+ *
+ * Return: void
  */
 void init_dog(struct dog *m, char *name, float age, char *owner)
 { 
