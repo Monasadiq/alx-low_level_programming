b26c797119824c7e31f60a008cd1ee9109ae9f27
monasadiq <mounsadiq759@gmail.com>
2023-04-18 07:17:50 -0700

    0x0e

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,10 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
+      if (d == NULL)
 		return;
 
-    free(d->owner);
-    free(d->name);
-    free(d);
+	free(d->owner);
+	free(d->name);
+	free(d);
 }
