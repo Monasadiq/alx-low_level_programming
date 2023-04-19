41178d6a6b75432850391bb552bd46baf9126228
monasadiq <mounsadiq759@gmail.com>
2023-04-19 16:38:07 -0700

    0x0d

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,13 @@
 #include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: string input
+ * @f: callback function
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
