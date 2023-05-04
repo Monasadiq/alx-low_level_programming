fc1c3c21f999bea69a1f92a80e3ee6eb91ebb314
Monasadiq <mounasadiq759@gmail.com>
2023-05-04 08:32:47 -0700

    0x03

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,29 +1,11 @@
-#include "main.h"
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
- *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
- */
-unsigned int binary_to_uint(const char *b)
-{
-	int i;
-	unsigned int num;
-
-	num = 0;
-	if (!b)
-		return (0);
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
-	return (num);
-}
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+#endif
